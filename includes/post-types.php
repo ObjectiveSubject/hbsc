19155191440649ba60d2d56f754c6c77e34157ad
Jonathan Auvray <jonathan.a@webitinteractive.ca>
2017-05-26 11:18:14 -0400

    whole bunch of updates

--- includes/post-types.php
+++ includes/post-types.php
@@ -11,20 +11,45 @@ function setup() {
 		return __NAMESPACE__ . "\\$function";
 	};
 
-	// NOTE: Uncomment to activate post type
-	// add_action( 'init', $n( 'register_my_post_type' ) );
+	add_action( 'init', $n( 'register_event' ) );
+	add_action( 'init', $n( 'register_salon' ) );
 
 }
 
 /**
- * Register the 'my_post_type' post type
+ * Register the 'event' post type
  *
  * See https://github.com/johnbillion/extended-cpts for more information
  * on registering post types with the extended-cpts library.
  */
-function register_my_post_type() {
-	register_extended_post_type( 'my_post_type', array(
-		'menu_icon' 		=> 'dashicons-layout',
-		'supports' 			=> array( 'title', 'editor', 'excerpt', 'thumbnail' ),
-	) );
+function register_event() {
+	register_extended_post_type( 'event', array(
+        'menu_icon'   => 'dashicons-calendar-alt',
+        'supports'    => array( 'title', 'editor', 'thumbnail' ),
+        //'has_archive' => true,
+        'public'      => true,
+        'dashboard_glance' => false,
+    ), array(
+        'singular' => __( 'Event' , 'hbsc' ),
+        'plural'   => __( 'Events' , 'hbsc' )
+    ) );
+}
+
+/**
+ * Register the 'salon' post type
+ *
+ * See https://github.com/johnbillion/extended-cpts for more information
+ * on registering post types with the extended-cpts library.
+ */
+function register_salon() {
+	register_extended_post_type( 'salon', array(
+        'menu_icon'   => 'dashicons-format-chat',
+        'supports'    => array( 'title', 'editor', 'thumbnail' ),
+        //'has_archive' => false,
+        'public'      => true,
+        'dashboard_glance' => false,
+    ), array(
+        'singular' => __( 'Salon' , 'hbsc' ),
+        'plural'   => __( 'Salons' , 'hbsc' )
+    ) );
 }
