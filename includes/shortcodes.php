ad9b8cfb8663b01ca0a9ecf5d7bed3a264f5f484
Jonathan Auvray <jonathan.a@webitinteractive.ca>
2017-05-30 11:51:06 -0400

    added module-row,  module-column shortcodes

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -12,6 +12,8 @@ function setup() {
 	};
 
 	add_shortcode( 'section-title', $n( 'section_title_func' ) );
+	add_shortcode( 'row', $n( 'row_func' ) );
+	add_shortcode( 'column', $n( 'column_func' ) );
 }
 
 /**
@@ -26,6 +28,37 @@ function setup() {
  */
 function section_title_func( $attributes, $content = null ) {
 	$content = preg_replace( '#^<\/p>|^<br \/>|<p>$#', '', $content );
-	$html = '<div class="section-title u-mt-4">' . do_shortcode( shortcode_unautop( trim( $content ) ) ) . '</div>';
+	$html = '<div class="module-title u-mt-4">' . do_shortcode( shortcode_unautop( trim( $content ) ) ) . '</div>';
+	return $html;
+}
+
+/**
+ * Create a row
+ *
+ * Example: [row]Content[/row]
+ *
+ * @param $attributes array List of attributes from the given shortcode
+ * @param $content mixed The content of the section
+ *
+ * @return mixed HTML output for the shortcode
+ */
+function row_func( $attributes, $content = null ) {
+	$content = preg_replace( '#^<\/p>|^<br \/>|<p>$#', '', $content );
+	$html = '<div class="module-row u-mt-2">' . do_shortcode( shortcode_unautop( trim( $content ) ) ) . '</div>';
+	return $html;
+}
+
+/**
+ * Create a column
+ *
+ * Example: [column]Content[/column]
+ *
+ * @param $attributes array List of attributes from the given shortcode
+ * @param $content mixed The content of the section
+ *
+ * @return mixed HTML output for the shortcode
+ */
+function column_func( $attributes, $content = null ) {
+	$html = '<div class="module-column">' . do_shortcode( shortcode_unautop( trim( $content ) ) ) . '</div>';
 	return $html;
 }
