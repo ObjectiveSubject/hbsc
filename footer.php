98a92202f478344ee6e91896cf01abbdb7ebc277
Jonathan Auvray <jonathan.a@webitinteractive.ca>
2017-06-02 16:29:39 -0400

    added fonts all around

--- footer.php
+++ footer.php
@@ -9,11 +9,11 @@
             <div class="u-container">
                 <div class="footer-row">
                     <div class="footer-column">
-                        <address>
+                        <address class="u-font-miller-bold">
                             <span class="u-display-block">77 Forest Street</span>
                             <span class="u-display-block">Hartford, CT 06105</span>
                         </address>
-                        <div class="contact-info u-mt-2">
+                        <div class="u-font-miller u-mt-2">
                             <span class="u-display-block"><a href="#">860-522-9258</a></span>
                             <span class="u-display-block"><a href="#">info@stowecenter.org</a></span>
                         </div>
@@ -54,7 +54,7 @@
                         </ul>
                     </div>
                     <div class="footer-column">
-                        <span>Newsletter</span>
+                        <span class="u-font-miller">Newsletter</span>
                         <form action="###" method="post" class="u-display-inline">
                             <input type="text">
                             <button class="button">Sign Up</button>
@@ -62,7 +62,7 @@
                     </div>
                 </div>
                 <div class="footer-row">
-                    <div class="footer-column">
+                    <div class="footer-column u-font-miller-bold u-caps u-font-size-xs u-color-tan">
                         <span class="copyright">&copy; <?php echo date('Y'); ?> HBSC</span>
                         <a href="#">Privacy Policy</a>
                         <a href="#">Terms of Use</a>
