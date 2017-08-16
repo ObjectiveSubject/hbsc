0cfab8c2501b37471f92ec69a814b3bd73c54f76
Jonathan Auvray <jonathan.a@webitinteractive.ca>
2017-08-16 14:13:50 -0400

    added modal for social sharing #52

--- partials/events/details-section.php
+++ partials/events/details-section.php
@@ -2,6 +2,47 @@
     $img = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large');
     $imgSrc = (isset($img[0]) ? $img[0] : '');
 ?>
+
+<div class="modal-overlay">
+    <div class="modal-container">
+        <div class="modal">
+            <div class="close-modal">
+                <a href="#" class="class-toggle" data-class="modal-active" data-target="body">
+                    <svg viewBox="0 0 1000 1000">
+                        <path d="M549,500L979.5,69.5c14-14,14-35,0-49c-14-14-35-14-49,0L500,451L69.5,20.5c-14-14-35-14-49,0c-14,14-14,35,0,49L451,500L20.5,930.5c-14,14-14,35,0,49c7,7,17.5,10.5,24.5,10.5c7,0,17.5-3.5,24.5-10.5L500,549l430.5,430.5c7,7,17.5,10.5,24.5,10.5c7,0,17.5-3.5,24.5-10.5c14-14,14-35,0-49L549,500z"/>
+                    </svg>
+                </a>
+            </div>
+            <div class="modal-content">
+                <div class="modal_item">
+                    <a href="#">
+                        <span class="icon icon-mail"></span>
+                        Email
+                    </a>
+                </div>
+                <div class="modal_item">
+                    <a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink();?>" target="_blank">
+                        <span class="icon icon-facebook"></span>
+                        Share
+                    </a>
+                </div>
+                <div class="modal_item">
+                    <a href="https://twitter.com/home?status=<?php echo urlencode(get_the_title().' '.get_permalink());?>" target="_blank">
+                        <span class="icon icon-twitter"></span>
+                        Tweet
+                    </a>
+                </div>
+            </div>
+            <div class="modal-content">
+                <div class="modal_item">
+                    <span class="u-font-miller-bold u-color-dark-gray">Permalink</span>
+                    <input type="text" value="<?php echo the_permalink(); ?>">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <section id="module-<?php echo $post->ID;?>" class="module module--hero has-background" <?php echo 'style="background-image: url(' . $imgSrc . ')"'; ?>>
     <div class="module__content u-container u-flex-justify-center" style="max-width:50.5rem;">
         <div class="card-positioner card-positioner-fullwidth">
@@ -65,7 +106,7 @@
                 <?php
                     }
                 ?>
-                    <a href="" class="button module-button u-mb-1">Share</a>
+                    <a href="#" class="button module-button class-toggle u-mb-1" data-class="modal-active" data-target="body">Share</a>
                 </div>
 
             </div>
