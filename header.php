c2c99ec3b909ab51776e2d3603cfcbf7af995a86
Jonathan Auvray <jonathan.a@webitinteractive.ca>
2017-08-21 13:59:56 -0400

    #25

--- header.php
+++ header.php
@@ -32,22 +32,34 @@ incrementViewsCount($post->ID);
         <div class="site-header u-bg-dark-gray">
             <div class="u-container">
                 <div class="site-header__content">
-                    <address class="u-display-block-md"><?php echo get_option('hbsc_address'); ?></address>
-                    <div class="u-display-block-md" style="z-index:50;">
-                        <a href="#" class="search-toggle class-toggle" data-target="#masthead" data-class="search-open" data-focus=".search-field"><span class="icon icon-search"></span></a>
+                    <address class="u-display-block-md">
+                        <a href="#" class="utility-toggle class-toggle" data-target="#masthead" data-class="utility-location-open">
+                            <?php echo get_option('hbsc_address'); ?>
+                        </a>
+                    </address>
+                    <div class="u-display-block-md">
+                        <a href="#" class="utility-toggle class-toggle" data-target="#masthead" data-class="utility-search-open" data-focus=".search-field"><span class="icon icon-search"></span></a>
                     </div>
                     <div class="open--hours">
-                        <?php
-                        $today = get_option('hbsc_hours_'.strtolower(date('l')));
-                        if( get_option('hbsc_closed_today') || empty($today) ) : ?>
-                        Closed Today
-                        <?php else : ?>
-                        Open Today: <?php hbsc_opening_hours_today(); ?>
-                        <?php endif; ?>
-                    </div>                    
+                        <a href="#" class="utility-toggle class-toggle" data-target="#masthead" data-class="utility-hours-open">
+                            <?php
+                            $today = get_option('hbsc_hours_'.strtolower(date('l')));
+                            if( get_option('hbsc_closed_today') || empty($today) ) : ?>
+                            Closed Today
+                            <?php else : ?>
+                            Open Today: <?php hbsc_opening_hours_today(); ?>
+                            <?php endif; ?>
+                        </a>
+                    </div>
                 </div>
-                <div class="site-search">
+                <div class="utility-item utility-item--location">
+                    Location
+                </div>
+                <div class="utility-item utility-item--search">
                     <?php get_search_form(); ?>
+                </div>
+                <div class="utility-item utility-item--hours">
+                    Hours
                 </div>
             </div>
         </div>
