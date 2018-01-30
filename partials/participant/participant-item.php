cc640350714c4d7691e666cec4f144bb4307468d
a-tawfik <taamer@gmail.com>
2018-01-30 13:12:52 -0500

    add year to prize winners #103

--- partials/participant/participant-item.php
+++ partials/participant/participant-item.php
@@ -14,8 +14,8 @@
 <?php
                 if( get_field('participant_winner') == 1 )
                 {
-?>                                    
-                    <div class="participant--winner">WINNER</div>
+?>
+                    <div class="participant--winner"><?php echo get_the_terms($post, 'participant-type')[0]->name? get_the_terms($post, 'participant-type')[0]->name : ""  ; ?> WINNER</div>
 <?php
                 }
 ?>
@@ -23,16 +23,16 @@
 <?php
                 if( get_field('participant_entry_type') == 'student' )
                 {
-?>                    
+?>
                         <div class="topic">Topic</div>
 <?php
                 }
-?>                        
+?>
                         <div class="entry--title"><?php echo get_field('participant_entry_title'); ?></div>
                         <div class="school--name"><?php echo get_field('participant_academic_institution'); ?></div>
                     </div>
 
-<?php 
+<?php
                 if( is_single() )
                 {
                     the_content();
